aed1f0e9b3a1d2e6962f935250b22659d6aac2fd
Bileonaire <kiokoleon@gmail.com>
2021-09-15 10:35:52 +0300

    Adjust port

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const routes = require('./routes');
+const PORT = process.env.PORT || 5000;
 
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 app.use(cors());
 
@@ -29,5 +30,5 @@ app.use((err, req, res, next) => {
     });
 });
 
-app.listen(5000, () => console.log('Quote API listening on port 5000! ---- http://localhost:5000/api/'));
+app.listen(PORT, () => console.log('Quote API listening on port 5000! ---- http://localhost:5000/api/'));
 
